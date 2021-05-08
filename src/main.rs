d6e08f55f2bcc581603c8fd0b0c8b34f8cbab85b
Ambrose Fok <left.righteous@gmail.com>
2021-05-09 11:46:08 +1200

    Trying to add a database

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use models;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +9,12 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+       .attach(SchedulerDbConn::fairing())
+       .mount("/", routes![index])
+    //    .mount("/events/create", routes![index])
+    //    .mount("/events/show", routes![index])
+    //    .mount("/events/edit", routes![index])
+    //    .mount("/events/signup", routes![index])
+       .launch();
 }
