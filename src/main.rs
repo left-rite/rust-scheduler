7541721e4aa58511d97714b34f90bf1088bd250a
Ambrose Fok <left.righteous@gmail.com>
2021-05-16 19:13:10 +1200

    create event page

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,26 @@ extern crate diesel;
 mod models;
 mod schema;
 
+use rocket_contrib::templates::Template;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/create")]
+fn create() -> Template {
+    Template::render("create_event", {})
 }
 
 fn main() {
     rocket::ignite()
        .attach(models::SchedulerDbConn::fairing())
        .mount("/", routes![index])
-    //    .mount("/events/create", routes![index])
+       .mount("/events", routes![create])
     //    .mount("/events/show", routes![index])
     //    .mount("/events/edit", routes![index])
     //    .mount("/events/signup", routes![index])
+       .attach(Template::fairing())
        .launch();
 }
